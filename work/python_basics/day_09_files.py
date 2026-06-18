26899292b93a9558f09de57b2154b46ecb4e4dc6
Axex-dev <dlirobloxagg@mail.ru>
2026-06-18 13:06:06 +0500

    2026_06_18 daily report

---
+++ work/python_basics/day_09_files.py
@@ -0,0 +1,14 @@
+import json
+
+with open("../data/students.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+def middle_grade(grades):
+    return sum(grades) / len(grades)
+
+
+with open("../results/grades_analysis.txt", "w", encoding="utf-8") as file:
+    file.write("Name:\t Grades:\t\t Avg. grade\n")
+    for student in data:
+        avg = middle_grade(student["grades"])
+        file.write(f"{student['name']} | {student['grades']} | {avg:.2f}\n")
