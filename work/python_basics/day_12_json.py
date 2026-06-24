2541409c9f1dcd78db084f29331fb9becd58d853
Axex-dev <dlirobloxagg@mail.ru>
2026-06-24 14:12:28 +0500

    2026_06_24 daily report and update day 12

--- work/python_basics/day_12_json.py
+++ work/python_basics/day_12_json.py
@@ -1,8 +1,9 @@
 import json
+import string
 
 zayavki = [
-    {"id": 1, "name": "Иван Иванов", "status": "В обработке"},
-    {"id": 2, "name": "Петр Петров", "status": "Одобрено"},
+    {"id": 1, "name": "Арсений Гномов", "status": "В обработке"},
+    {"id": 2, "name": "Гном Арсений", "status": "Одобрено"},
     {"id": 3, "name": "Анна Смирнова", "status": "Отклонено"},
 ]
 
@@ -13,3 +14,12 @@ with open("../data/request.json","r", encoding="utf-8") as f:
     requests = f.read()
     for request in json.loads(requests):
         print(f"Номер: {request['id']} | Имя: {request['name']} | Статус: {request['status']}")
+
+def validator_request(_request):
+    for row in _request:
+        if row["id"] == "" or row["name"] == "" or row["status"] == "":
+            print("Не все поля заполнены.")
+            return False
+    return None
+
+print(validator_request(zayavki))
