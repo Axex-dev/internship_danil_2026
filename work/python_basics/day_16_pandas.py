b4ae8e6f129f273887bdc15615faf59a2eb55821
Axex-dev <dlirobloxagg@mail.ru>
2026-06-30 13:29:35 +0500

    2026_06_30 daily report

--- work/python_basics/day_16_pandas.py
+++ work/python_basics/day_16_pandas.py
@@ -1,7 +1,29 @@
 import pandas as pd
 
+# Открыть CSV через pandas
 df = pd.read_csv("../data/pandas.csv")
+
+
+# Вывести первые 5 строк
+print("Первые 5 строк:")
 print(df.head())
-print("Количество строк и столбцов:", df.shape)
+
+
+# Посчитать строки и столбцы
+print("Количество строк и столбцов:")
+print(df.shape)
+
+
+# Вывести список столбцов
 print("Столбцы:")
 print(df.columns.tolist())
+
+
+# Найти пропуски
+print("Пропуски:")
+print(df.isnull().sum())
+
+
+# Найти дубли
+print("Количество дублей:")
+print(df.duplicated().sum())
